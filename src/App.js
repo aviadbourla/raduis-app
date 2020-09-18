41940378ccfdf7109d05406fddc0ee5038daeb14
aviadbourla <48490695+aviadbourla@users.noreply.github.com>
2020-09-18 12:51:42 +0300

    css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ShowMap from './Map/ShowMap';
 import './App.css';
 import ButtonAppBar from './Nav/ButtonAppBar'
-
 function App() {
   return (
     <div>
--- src/Nav/ButtonAppBar.js
+++ src/Nav/ButtonAppBar.js
@@ -3,9 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Link from '@material-ui/core/Link';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -24,7 +21,6 @@ const useStyles = makeStyles((theme) => ({
 // 
 export default function ButtonAppBar() {
     const classes = useStyles();
-
     return (
         <AppBar position="relative">
             <Toolbar>
